aa4a751cf73a6a29f62a6c591552f683e1955c8f
naxit-01 <bzatekv@gmail.com>
2024-01-24 16:46:45 +0100

    clear the repo

--- pokus.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call opensslstart.bat && openssl version
-if %errorlevel%==0 (
-    echo Both commands executed successfully.
-) else (
-    echo Failed to execute one or both commands.
-)
-pause
-
